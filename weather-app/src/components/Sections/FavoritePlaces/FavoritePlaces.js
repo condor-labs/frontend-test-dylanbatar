44a6039087e16e6ae3127fd25c4de8de8345500d
dylanbatar <dylanbatar2@gmail.com>
2020-11-13 23:00:30 -0500

    Structure widtget #2,3,4 and 5

---
+++ weather-app/src/components/Sections/FavoritePlaces/FavoritePlaces.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const FavoritePlaces = () => {
+  return (
+    <section>
+      <h2>Our favorite places</h2>
+    </section>
+  );
+};
--- weather-app/src/components/Sections/Slide/Slide.js
+++ weather-app/src/components/Sections/Slide/Slide.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { forecastExtended } from "../../../api/forecasteExtended";
+import { citiesPrognostic } from "../../../api/cities";
 import { ContainerCard } from "../../Cards/ContainerCard/ContainerCard";
+import { useFetch } from "../../../hooks/useFetch";
 
 export const Slide = ({ slideItems = [] }) => {
+  const [sliderData, setSlideItems] = useState(slideItems);
+  const weatherDay = useFetch(citiesPrognostic);
+
+  useEffect(() => {
+    console.log(weatherDay);
+  }, []);
+
   return (
     <ContainerCard>
       <div id="widget-carousel"></div>
